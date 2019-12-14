714cb630cc317df5bffaa2b72f5f9ade207d090d
segmentationfaulter <muhammadsackib@gmail.com>
2019-12-14 09:27:42 +0500

    add vscode syntax highligher

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,15 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          `gatsby-remark-prismjs`,
+          {
+            resolve: `gatsby-remark-vscode`,
+            options: {
+              extensions: [{
+                identifier: 'Elmtooling.elm-ls-vscode',
+                version: '0.7.3'
+              }]
+            }
+          },
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
         ],
